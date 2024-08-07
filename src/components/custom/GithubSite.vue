931cb2376ef4f987af1c5f1da26e293f469e0923
rjl888 <146054914+rjl888@users.noreply.github.com>
2024-08-07 18:26:36 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+     <span>RJLAPI</span>
+    <a href="https://api.raojialong.space" target="_blank" class="text-blue-500">
+      RJLAPI
     </a>
   </div>
 </template>
